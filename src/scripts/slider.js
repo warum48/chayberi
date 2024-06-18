1dc9ace9bafd2531b20a51cd867c24177a2a895f
Vadim Rumyantsev <warum48@gmail.com>
2024-06-19 00:52:34 +0700

    more sections

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,13 +1,21 @@
 //<script type="module">
   //import {tns} from './tiny-slider.js';
 
-  var slider = tns({
+ var slider = tns({
     container: '.my-slider',
     items: 4,
     gutter: 10,
     slideBy: 'page',
     autoplay: true,
-   // mode : 'gallery',
-  //  autoWidth: true
+  }); 
+
+ 
+  var slider2 = tns({
+    container: '.my-slider2',
+    items: 4,
+    gutter: 10,
+    slideBy: 'page',
+    autoplay: true,
   });
+ 
 //  </script>
