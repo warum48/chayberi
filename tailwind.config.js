acff6bbfe256b8bcab6649545425ee5fa464a349
Vadim Rumyantsev <warum48@gmail.com>
2024-06-20 01:19:38 +0700

    more details

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,16 @@ module.exports = {
   theme: {
     extend: {
       letterSpacing: {
+        super: '.425em',
         ultra: '.5em',
       },
       fontFamily: {
         'montserrat': ['Montserrat'],// ...defaultTheme.fontFamily.sans],
         'inter': ['Inter'],//, ...defaultTheme.fontFamily.sans],
         'marianna' : ['Marianna']
+      },
+      fontSize: {
+        h2: '34px',
       }
     },
     colors: {
@@ -22,6 +26,8 @@ module.exports = {
       current: 'currentColor',
       gray: colors.gray,
       'chalet-green': '#5A6B36',
+     // chalet_green: '#5A6B36',
+    //  testgreen: '#556836',
       'champagne': '#FBECCE',
 
 	    'gimblet': '#afb871',
@@ -29,6 +35,7 @@ module.exports = {
 	    'fuscuos-gray': '#4c4c4c',
 	    'yellowfish-orange': '#ffa915',
 	    'silver-sand': '#c0c1bc',
+     // 'ssilver-sand': '#c0c1bc',
 
     },
   },
