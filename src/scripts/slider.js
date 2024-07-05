5f6180204a558f9fe4c4b108daed6af9149533e5
Vadim Rumyantsev <warum48@gmail.com>
2024-07-05 23:02:40 +0700

    first demo

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -7,12 +7,22 @@
     gutter: 10,
     slideBy: 'page',
     autoplay: true,
+    controlsText: ['<div class="product-slider-control psc-left"></span>', '<div class="product-slider-control psc-right"></span>'],
   }); 
 
  
   var slider2 = tns({
     container: '.my-slider2',
     items: 4,
+    gutter: 10,
+    slideBy: 'page',
+    autoplay: true,
+    controlsText: ['<div class="product-slider-control psc-left"></span>', '<div class="product-slider-control psc-right"></span>'],
+  });
+
+  var slider3 = tns({
+    container: '.reviewSlider',
+    items: 1,
     gutter: 10,
     slideBy: 'page',
     autoplay: true,
